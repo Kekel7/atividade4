d7fa1d51066146b5b5b4097d054233d872b0aabb
kekel7 <kellmys.sz.ribeiro@gmail.com>
2022-07-03 15:59:50 -0300

    chrome

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import './App.css';
+
+function App(){
+
+return(
+ 
+ <div className='container'>
+        
+ <div className='box'>
+   
+   <div className='img'>
+     <img src="https://portal.ifba.edu.br/eunapolis/imagens-campus-eunapolis/icone-suap.jpg/@@images/936f39f8-14ec-4861-9f1f-35de0ec4fa29.jpeg"/>
+   </div>
+   Inicio
+
+ </div>
+ <div className='box'>
+ <div className='img'>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1zRaB-sN4xL75VeNwivt
+     StT0rRKyZBauobQ&usqp=CAU"/>
+   </div>
+   globo.com 
+
+ </div>
+ <div className='box'>
+ <div className='img'>
+     <img src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png"/>
+   </div>
+   Facebook
+ </div>
+ <div className='box'>
+ <div className='img'>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/CUPS.svg/125px-CUPS.svg.png"/>
+
+   </div>
+   Home
+ </div>
+ <div className='box'>
+   <div className='img'>
+       <img src="https://scontent.fgjm1-1.fna.fbcdn.net/v/t1.18169-9/298556_656920890991958_1943558491_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cHaOO8Ca4B8AX8vO1Pd&_nc_ht=scontent.fgjm1-1.fna&oh=00_AT-KquPH3hOrfWOyR4st8kVmkU2dIh6LdPYawzyClao9Jw&oe=62E6190D"/>
+   </div>
+   Mirim 
+ </div>
+ <div className='box'>
+   <div className='img'>
+   <img src="https://yt3.ggpht.com/wa3WhBsrLCeQim1OArpXrLf0L5C2hNV9j84maZn9LiPAlf5pF7axqYsws7RcZ9nsXwT1JIbiWzw=s88-c-k-c0x00ffffff-no-rj"/>
+   </div>
+   AVA
+ </div>
+ <div className='box'>
+   <div className='img'>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgK-tHxQJjR-nhS_Il1Izzcs7VIiNI1V47_Q&usqp=CAU"  />
+   </div>
+   SEI/IFRO
+ </div>
+ <div className='box'>
+   <div className='img'>
+     <img src="https://cdn.icon-icons.com/icons2/2631/PNG/512/gmail_new_logo_icon_159149.png"  />
+   </div>
+   Gmail
+ </div>  
+ <div className='box'>
+   <div className='img'>
+       <img src="https://yt3.ggpht.com/wa3WhBsrLCeQim1OArpXrLf0L5C2hNV9j84maZn9LiPAlf5pF7axqYsws7RcZ9nsXwT1JIbiWzw=s88-c-k-c0x00ffffff-no-rj"/>
+   </div>
+   Mirim 
+ </div>
+ <div className='box'>
+   <div className='img'>
+     <img src="https://casajnakao.vteximg.com.br/arquivos/ids/180835-100-100/132300_2.jpg?v=637378533633170000"  />
+   </div>
+   Add short...  
+ </div>  
+</div>
+);
+}
+export default App;
